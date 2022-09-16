f81e69a949999948bf4393d5f000d70be760529b
tigi44 <onlytg@naver.com>
2022-09-16 17:36:57 +0900

    Initial Commit

---
+++ LiveActivitiesExample/LiveActivitiesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivitiesExampleApp.swift
+//  LiveActivitiesExample
+//
+//  Created by tigi KIM on 2022/09/16.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivitiesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
